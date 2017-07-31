2e59385f64198531bf6e491a0b1c11ca221ce034
ljylhm <948021695@qq.com>
2017-07-31 23:23:27 +0800

    'pat-test-01'

--- src/second/Three.java
+++ src/second/Three.java
@@ -1,11 +1,9 @@
 package second;
 
-import java.security.Timestamp;
-
 public class Three {
     int time;
+    //运用递归的方法
 	public int getTimes(int number,int times){
-		System.out.println(time);
 		if(number>1){
 			if(number%2==0){
 		         number = number/2;
@@ -25,11 +23,31 @@ public class Three {
 		return time;
          
    }
+     //运用while的方法
+	public int getTimesByWhile(int number){
+		int times = 0;
+		boolean isOne = number>1?true:false;
+		while(isOne){
+		 if(number%2==0){
+			times = times+1;
+			number = number/2;
+			isOne = number==1?false:true;
+		 }else{
+			 times = times+1;
+			 number=(number*3+1)/2;
+			 isOne = true;
+		 } 
+		 
+		}
+		return times;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 	    Three three = new Three();
-	    int times = three.getTimes(3,0);
+	    int times = three.getTimesByWhile(3);
+	   /* int times = three.getTimes(3,0);*/
 	    System.out.println(times);
         
 	}
